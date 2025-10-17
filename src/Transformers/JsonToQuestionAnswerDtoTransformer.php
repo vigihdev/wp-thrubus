77beb978baa537e4bb46d04963b4d58cc31fe0fd
vigih dev <vigihdev@gmail.com>
2025-10-17 20:02:25 +0700

    ✨ 17-10-2025 12-19

---
+++ src/Transformers/JsonToQuestionAnswerDtoTransformer.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WpThrubus\Transformers;
+
+use WpThrubus\DTOs\QuestionAnswerDto;
+
+final class JsonToQuestionAnswerDtoTransformer extends AbstractJsonTransformer
+{
+
+    /**
+     * Transform single JSON string to QuestionAnswerDto
+     * 
+     * @return QuestionAnswerDto|null
+     */
+    public function transformJson(string $json): ?QuestionAnswerDto
+    {
+
+        if (!$this->validateJson($json)) {
+            return null;
+        }
+
+        try {
+            return $this->serializer->deserialize($json, QuestionAnswerDto::class, 'json');
+        } catch (\Throwable $e) {
+            error_log("Failed to deserialize: " . $e->getMessage());
+            return null;
+        }
+    }
+
+
+    /**
+     * Transform single JSON string containing array to array of QuestionAnswerDto
+     * 
+     * @return QuestionAnswerDto[]
+     */
+    public function transformArrayJson(string $json): array
+    {
+
+        if (!$this->validateArrayJson($json)) {
+            return [];
+        }
+
+        try {
+            return $this->serializer->deserialize($json, QuestionAnswerDto::class . '[]', 'json');
+        } catch (\Throwable $e) {
+            error_log("Failed to deserialize: " . $e->getMessage());
+            return [];
+        }
+    }
+
+    /**
+     * Transform JSON file content to array of QuestionAnswerDto
+     * 
+     * @return QuestionAnswerDto[]|QuestionAnswerDto|null
+     */
+    public function transformFileJson(string $filePath): array|null|QuestionAnswerDto
+    {
+        return $this->transformFromFile($filePath);
+    }
+}
